9edefe10e9fb46b6ff3ea57ab7b5a24b8c526481
Charlotte Njoh <ncharlotte8@gmail.com>
2022-10-30 21:50:17 -0400

    python playgroung

---
+++ PYTHON/Plaground/accountid.py
@@ -0,0 +1,3 @@
+account_arn = "arn:aws:iam::123456789012:user/Developement/product_1234/*"
+account_id = account_arn[13:25]
+print(account_id)
---
+++ PYTHON/Plaground/weight.convertor.py
@@ -0,0 +1,4 @@
+user_weight_lbs = float(input("what is your weight in lbs?: "))
+user_weight_kgs = user_weight_lbs * 0.453
+
+print(f"user weight in kgs is {user_weight_kgs} kgs")
