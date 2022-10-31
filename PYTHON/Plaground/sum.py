da883a08995483486af5b73062821eb09dcd5b9f
Charlotte Njoh <ncharlotte8@gmail.com>
2022-10-30 21:30:18 -0400

    test

---
+++ PYTHON/Plaground/sum.py
@@ -0,0 +1,5 @@
+year_1 = int(input("the year you came to the USA:\n"))
+year_2 = int(input("the number of years in this country:\n"))
+current_year = year_1 + year_2
+
+print(f"current year is: {current_year}")
