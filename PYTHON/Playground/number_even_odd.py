63873211e7851b36be9f27a88fb751cd8b167f8f
Charlotte Njoh <ncharlotte8@gmail.com>
2022-11-01 16:16:29 -0400

    restoring

---
+++ PYTHON/Playground/number_even_odd.py
@@ -0,0 +1,7 @@
+# This program will ask the users for a number and will check if it is Even or Odd.
+number = int(input("enter a number between 1 and 100: \n"))
+if (number % 2) == 0:
+    print(f"the number you entered {number} is Even")
+else:
+    print(f"the number you entered {number} is Odd")
+    
---
+++ PYTHON/Playground/reverse.py
@@ -0,0 +1,3 @@
+word = input("what is your favorite color? ")
+reverse_word = word[::-1]
+print(f"what is the reverse word of your favorite color? {reverse_word}")
