9cad4aca53ce531bb6c47a041cba1e5a0e59074a
Charlotte Njoh <ncharlotte8@gmail.com>
2022-11-10 13:39:14 -0500

    Min and max

---
+++ PYTHON/Playground/min_max.py
@@ -0,0 +1,20 @@
+# my_list = int(input("enter your list: \n"))
+
+
+list_1 = [3, 4, 5, 6]
+list_2 = [-1, -2, -3, -4]
+list_3 = [0, 0, 0, 0]
+list_4 = []
+
+print(f"{max(list_1)} {min(list_1)}\n{max(list_2)} {min(list_2)}\n{max(list_3)} {min(list_3)}")
+
+if len(list_4) == 0:
+    print([])
+
+
+
+
+    
+
+
+
